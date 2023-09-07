4376e0962da27ac79deafa00e315809bcb596fcc
Pacatro <paco.algar.15@gmail.com>
2023-09-07 13:01:44 +0200

    fixing things

--- server/routes/tasks.py
+++ server/routes/tasks.py
@@ -8,6 +8,11 @@ async def tasks(user_id: str):
     return get_tasks(user_id)
 
 
+@router.get("/task/{task_id}")
+async def task(task_id: int):
+    return get_task(task_id)
+
+
 @router.post("/{user_id}", status_code=status.HTTP_201_CREATED)
 async def tasks(user_id: str, task_data: dict):
     if not insert_task(user_id, task_data):
@@ -28,7 +33,3 @@ async def tasks(task_id: int):
     if not delete_task(task_id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Can't remove new task")
     return {"message": "Task deleted"}
-
-@router.get("/task/{task_id}")
-async def task(task_id: int):
-    return get_task(task_id)
