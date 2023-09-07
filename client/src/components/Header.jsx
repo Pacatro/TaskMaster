cd5866e948cacd7fddfb310da87cde810741f847
Pacatro <paco.algar.15@gmail.com>
2023-09-07 17:35:03 +0200

    adding fornted

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>TaskMaster</h1>
+      <div>
+        <button>Log in</button>
+        <button>Sign up</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
