870430fd12e32bed0876571eddd687f156860504
Pacatro <paco.algar.15@gmail.com>
2023-09-14 16:42:25 +0200

    adding logout

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,19 +4,29 @@ import API from "../api/api";
 
 function Home() {
   const [userData, setUserData] = useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const getUser = async () => {
     const data = await API.getUserData();
     setUserData(data);
   };
 
+  const logout = () => {
+    localStorage.removeItem("access_token")
+    navigate('/')
+  }
+
   useEffect(() => {
-    const token = localStorage.getItem("access_token")
-    token ? getUser() : navigate('/');
+    const token = localStorage.getItem("access_token");
+    token ? getUser() : navigate("/");
   }, []);
 
-  return <h1>Welcome {userData?.name}</h1>;
+  return (
+    <>
+      <h1>Welcome {userData?.name}</h1>
+      <button onClick={logout}>Logout</button>
+    </>
+  );
 }
 
 export default Home;
